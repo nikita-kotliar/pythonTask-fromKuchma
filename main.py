94fd88d415c85f0f9bf9d4ae0b0d9a7b749e47c6
YaroslavaSegal <149417781+YaroslavaSegal@users.noreply.github.com>
2023-12-26 20:39:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,42 @@ from datetime import date, datetime
 
 
 def get_birthdays_per_week(users):
-    # Реалізуйте тут домашнє завдання
-    return users
+    if users == []:
+        return list()
+        break
+    current_date = date.today()
+    current_year = current_date.year
+    celebrate = {'Monday': [], 'Tuesday': [], 'Wednesday': [], 'Thursday': [], 'Friday': []}
+    for user in users:
+        user_date = user['birthday'].replace(year=current_year)
+        # check all birthdays in current year
+        if timedelta(days=0) <= (user_date - current_date) <= timedelta(days=7):
+            if user_date.weekday() == 0 or user_date.weekday() == 5 or user_date.weekday() == 6:
+                celebrate['Monday'].append(user['name'])
+            elif user_date.weekday() == 1:
+                celebrate['Tuesday'].append(user['name'])
+            elif user_date.weekday() == 2:
+                celebrate['Wednesday'].append(user['name'])
+            elif user_date.weekday() == 3:
+                celebrate['Thursday'].append(user['name'])
+            elif user_date.weekday() == 4:
+                celebrate['Friday'].append(user['name'])
+
+        user1_date = user['birthday'].replace(year=current_year + 1)
+        # check all birthdays in next year
+        if timedelta(days=0) <= (user1_date - current_date) <= timedelta(days=7):
+            if user1_date.weekday() == 0 or user1_date.weekday() == 5 or user1_date.weekday() == 6:
+                celebrate['Monday'].append(user['name'])
+            elif user1_date.weekday() == 1:
+                celebrate['Tuesday'].append(user['name'])
+            elif user1_date.weekday() == 2:
+                celebrate['Wednesday'].append(user['name'])
+            elif user1_date.weekday() == 3:
+                celebrate['Thursday'].append(user['name'])
+            elif user1_date.weekday() == 4:
+                celebrate['Friday'].append(user['name'])
+
+    return celebrate
 
 
 if __name__ == "__main__":
