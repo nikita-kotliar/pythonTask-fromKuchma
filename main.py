7557619534bf1b908fabd10820a4aaef33fa78db
nikita-kotliar <knr200710@gmail.com>
2024-12-20 14:09:28 +0200

    Ready task from Kuchma

--- main.py
+++ main.py
@@ -1,9 +1,48 @@
-from datetime import date, datetime
-
+from datetime import date, timedelta, datetime
 
 def get_birthdays_per_week(users):
-    # Реалізуйте тут домашнє завдання
-    return users
+    today = date.today()
+    # print(today)
+    # дата понеділка
+    monday_this_week = today - timedelta(days=today.weekday())
+    # дата п'ятниці 
+    friday_this_week = monday_this_week + timedelta(days=4)
+    # початок наступного тижня
+    monday_next_week = monday_this_week + timedelta(days=7)
+
+    birthdays_by_day = {
+        "Monday": [],
+        "Tuesday": [],
+        "Wednesday": [],
+        "Thursday": [],
+        "Friday": [],
+    }
+
+    for user in users:
+        birthday = user["birthday"]
+        birthday = birthday.replace(year=today.year)
+
+        # Якщо день народження вже пройшов
+        if birthday < today:
+            birthday = birthday.replace(year=today.year + 1)
+
+        weekday = birthday.weekday()
+        # print(weekday)
+
+        # переносиння на понеділок
+        if weekday == 5 or weekday == 6:  
+            birthday = birthday + timedelta(days=(7 - weekday))
+            weekday = 0  
+
+        # додавання дня
+        if monday_this_week <= birthday <= friday_this_week or monday_next_week <= birthday < monday_next_week + timedelta(days=5):
+            # print (birthday)
+            day_name = list(birthdays_by_day.keys())[weekday]
+            birthdays_by_day[day_name].append(user["name"])
+
+    # print ({day: names for day, names in birthdays_by_day.items() if names})
+    return {day: names for day, names in birthdays_by_day.items() if names}
+
 
 
 if __name__ == "__main__":
